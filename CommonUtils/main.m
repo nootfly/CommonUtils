807a23114236d3c7ff8f8866d1a4b0ab80fcd2bd
Noot <nootfly@gmail.com>
2013-01-08 17:22:03 +1100

    Initial Commit

---
+++ CommonUtils/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CommonUtils
+//
+//  Created by Kheam on 8/01/13.
+//  Copyright (c) 2013 Noot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
